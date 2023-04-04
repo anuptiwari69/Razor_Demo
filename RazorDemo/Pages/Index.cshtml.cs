dec2c92aeaa2e23a3fbfddbea4c9037d97caaeff
anuptiwari69 <57125921+anuptiwari69@users.noreply.github.com>
2023-04-04 09:30:06 +0530

    Removed list of users from index

--- RazorDemo/Pages/Index.cshtml.cs
+++ RazorDemo/Pages/Index.cshtml.cs
@@ -5,37 +5,13 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using RazorDemo.Data;
 using RazorDemo;
 using System.Security.Claims;
-using System.Threading.Tasks;
+
 
 public class IndexModel : PageModel
 { 
- private readonly DataContext _db;
-
-public IndexModel(DataContext db)
-{
-    _db = db;
-}
-
-public List<User> Users { get; set; }
 
 
 
-
-
-
-public ActionResult OnGet()
-{
-    ClaimsPrincipal ClaimUser = HttpContext.User;
-    if (ClaimUser.Identity.IsAuthenticated)
-        Users = _db.Users.ToList();
-
-
-    return Page();
-
-
-
-}
-    
 
     public async Task<IActionResult> OnPostLogoutAsync()
     {
